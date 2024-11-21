a5d36f36fbb189fcbaf8591a0f54aff99faeca38
Etienne Chollet <etiennepchollet@gmail.com>
2024-11-21 16:16:09 -0500

    [DOCS] Format in compliance with mkdocs

--- SynthShapes/__init__.py
+++ SynthShapes/__init__.py
@@ -1,1 +1,28 @@
-# core/__init__.py
+"""
+This package is a versatile deep-learning toolbox for PyTorch,
+tailored to researchers working with N-dimensional vision problems,
+and more specifically medial imaging problems.
+
+It is intended to provide building blocks for a wide variety of
+architectures, as well as a set of pre-defined backbones, as well as
+a few task-specific models (segmentation, registration, synthesis, ...).
+
+It will not provide domain-specific tools with dedicated pre- and post-
+processing pipelines. However, such high-level tools can be implemented
+using this toolbox.
+
+Modules
+-------
+blending
+    Task-specific models.
+filters
+    Task-agnostic architectures to use as backbones in models.
+hist
+    Building blocks for backbones and models.
+shapes
+    Differentiable functions to optimize during training.
+texturizing
+    Non-differentiable functions to compute during validation.
+utils
+    Tools to train networks.
+"""
---
+++ docs/api/shapes.md
@@ -0,0 +1,1 @@
+# ::: SynthShapes.shapes
